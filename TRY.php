30e35b3143f03dd7156ee6a117b5397e25e2fd11
Alone Ghost <67594230+PHP17236@users.noreply.github.com>
2020-06-29 18:19:50 +0300

    A easy PHP code everyone can do right?
    Yup it is a  easy PHP code everyone can code.

---
+++ TRY.php
@@ -0,0 +1,10 @@
+<form method="post">
+<input type="text" name="name" placeholder="name">
+<input type="submit" value="send it!>
+</form>
+<?php
+if($_POST)
+{
+echo"what do you want $name?";
+}
+?>
